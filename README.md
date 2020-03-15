eafc2a34e6af07518696b3d194b2ac60aa194f60
Flavien Lambert <PetitLepton@users.noreply.github.com>
2020-03-15 18:39:05 +0100

    Add wrapping class
    This MR brings a new class encapsulating the usage of the package. Type hints are added and a couple of unit tests on single functions.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-The following script allows to extract the packages from a conda environment file like
+The following module allows to extract the packages from a conda environment file like
 ```yaml
 name: analytics
 channels:
@@ -15,17 +15,23 @@ dependencies:
 ```
 and check if potential updates for the packages are available on PyPi. The code below shows an example of usage
 ```python
-from pypi_releases import packages_from_conda_env_file, get_updates
+from pypi_releases import CondaEnvironment
 
-if __name__ == "__main__":
-    packages = packages_from_conda_env_file(conda_file_name="analytics.yml")
-    updates = get_updates(packages=packages)
-    versions = ";".join(
-        [
-            f"\n- {package}: {version} \u2192 {new_version}"
-            for package, version, new_version in updates
-        ]
-    )
-    print(f"These new PyPi versions have been released: {versions}.")
-
+conda_environment = CondaEnvironment(file_path="./analytics.yml")
+latest_versions = conda_environment.get_all_latest_versions()
+versions = ";".join(
+    [
+        f"\n- {package}: {version} \u2192 {new_version}"
+        for package, version, new_version in latest_versions
+    ]
+)
+print(f"These new PyPi versions have been released: {versions}.")
+```
+leading to
+```
+These new PyPi versions have been released: 
+- notebook: 5.4.1 → 6.0.3;
+- pandas: 0.23.4 → 1.0.2;
+- plotly: 3.4.0 → 4.5.4;
+- datasheets: 0.1.0 → 0.3.0.
 ```
--- pypi_releases.py
+++ pypi_releases.py
@@ -1,64 +1,172 @@
+from typing import Dict, List, Tuple
+import pathlib
 import requests
 import yaml
 
 
-def packages_from_conda_env_file(conda_file_name):
-    """Extract the packages from a YAML Conda environment
+def read_conda_file(file_path: str) -> str:
+    """Returns the content of a conda environment file.
+    
+    Parameters
+    ----------
+
+    file_path: str
+        Path to the conda environment file
+        
+    Returns
+    -------
+
+    str
+        Content of the file"""
+
+    if not isinstance(file_path, str):
+        raise TypeError("The path to the conda file should be a string.")
+    return pathlib.Path(file_path).read_text()
+
+
+def extract_package_name_and_version(line: str) -> Tuple[str, str]:
+    """Returns the name and the version of the package. If the version is not
+    provided, it is set to Not provided.
+    
+    Parameters
+    ----------
+
+    package_line: str
+        Line extracted from the conda environment file of the form package=version or package==version
+            
+    Returns
+    -------
+
+    Tuple[str, str] 
+        Name and version of the package"""
+
+    split_line = line.split("=")
+    name = split_line[0]
+    # It is mandatory to use the last element of the list
+    # for the version as pip packages are written name==version
+    version = split_line[-1] if len(split_line) > 1 else "Not provided"
+
+    return (name, version)
+
+
+def extract_all_packages_names_and_versions(file_content: str) -> List:
+    """Returns the list of packages names and version from a conda environment
     file.
-    See https://conda.io/docs/user-guide/tasks/manage-environments.html#creating-an-environment-from-an-environment-yml-file"""
-    with open(conda_file_name, "r") as f:
-        content = yaml.load(f)
 
-    dependencies = content["dependencies"]
-    packages = list()
+    Parameters
+    ----------
+
+    file_content: str 
+        Content of the environment file
+
+    Returns
+    -------
+
+    List[Tuple[str, str]]
+        List of the names and versions of the packages"""
+
+    parsed_content = yaml.safe_load(file_content)
+    dependencies = parsed_content["dependencies"]
+
+    packages_names_and_versions = []
+
     for p in dependencies:
-        if type(p) == str:
-            name = p.split("=")[0]
-            version = p.split("=")[-1]
-            if name != "python":
-                packages.append((name, version))
-        if type(p) == dict:
-            pip_dependencies = p["pip"]
-            packages += [(p.split("=")[0], p.split("=")[-1]) for p in pip_dependencies]
 
-    return dict(packages)
+        # Catch the conda packages
+        if isinstance(p, str):
+            packages_names_and_versions += [p]
+
+        # Catch the pip packages
+        if isinstance(p, dict):
+            packages_names_and_versions += p["pip"]
+
+    return packages_names_and_versions
 
 
-def get_latest_release(package):
-    """Get the latest release version and date of a package
-    on Pypi by using the PyPi API. The version date corresponds
-    to the latest upload.
+def get_latest_version(package_name: str) -> str:
+    """Get the latest release version of a package on PyPi by using the PyPi
+    API.
 
-    Args:
-        package (str): name of the pacckage
-    Returns:
-        dict(version=(str), verion_date=str())"""
+    Parameters
+    ----------
 
-    url = "https://pypi.org/pypi/{0}/json".format(package)
-    response = requests.get(url=url)
+    package_name: str
+        Name of the package
+
+    Returns
+    -------
+
+    str
+        Latest version of the package"""
+
+    response = requests.get(url=f"https://pypi.org/pypi/{package_name}/json")
 
     if response.ok:
         content = response.json()
-        version = content.get("info").get("version")
-        releases = content.get("releases")
-        version_date = max(
-            (list(upload["upload_time"] for upload in releases[version]))
-        )
-        latest_release = dict(version=version, version_date=version_date)
+        return content.get("info").get("version")
     else:
-        latest_release = None
-    return latest_release
+        raise requests.ConnectionError()
 
 
-def get_updates(packages):
-    """"""
-    updates = list()
-    for package in packages.keys():
-        version = packages[package]
-        release = get_latest_release(package=package)
-        if release:
-            release_version = release["version"]
-            if version != release_version:
-                updates.append((package, version, release_version))
+def get_all_latest_versions(packages_names_and_versions: List[Tuple[str, str]]) -> List:
+    """Returns the latest releases for a list of given packages
+    
+    Parameters
+    ----------
 
-    return updates
+    packages_names_and_versions: List[Tuple[str, str]]
+        List of the tuples (name, version) of the packages
+        
+    Returns 
+    -------
+
+    List[Tuple[str, str, str]]
+        List of the tuples (name, version, latest_version)"""
+
+    new_releases = []
+    for name, version in packages_names_and_versions:
+        latest_version = get_latest_version(package_name=name)
+        if latest_version != version:
+            new_releases.append((name, version, latest_version))
+
+    return new_releases
+
+
+class CondaEnvironment:
+    """Class conveniently wrapping the steps to extract the latest versions of
+    the packages for a given conda environment file.
+
+    Attributes
+    ----------
+    file_path: str
+        Path to the conda environment file
+
+    Methods
+    -------
+
+    get_all_latest_versions()
+        Returns the latest versions of all the packages in the environment file
+        if those are different from the current ones
+    
+    """
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.packages = []
+
+        file_content = read_conda_file(file_path=self.file_path)
+
+        for line in extract_all_packages_names_and_versions(file_content=file_content):
+            name, version = extract_package_name_and_version(line=line)
+            # Do not add python as a package
+            if name != "python":
+                self.packages.append((name, version))
+
+    def __repr__(self):
+        versions = ";".join(
+            [f"\n- {name}: {version}" for name, version in self.packages]
+        )
+        return f"List of packages in the environment file:{versions}."
+
+    def get_all_latest_versions(self):
+        return get_all_latest_versions(packages_names_and_versions=self.packages)
